eb0129ba0c5fc4b93d352e8d47f95674f8613e0b
Sarder Mohiuddin <mohin700@gmail.com>
2017-09-18 17:23:48 +0600

    Update WooCommerce.php

--- WooCommerce.php
+++ WooCommerce.php
@@ -1,4 +1,13 @@
+<?php
+
+
 //Additional Fees 
 //Code will be add in to => Functions.php
 
 
+function woo_add_cart_fee() {
+  global $woocommerce;
+  $woocommerce->cart->add_fee( __('Registration Fee', 'woocommerce'), 35 );
+}
+
+add_action( 'woocommerce_cart_calculate_fees', 'woo_add_cart_fee' );
